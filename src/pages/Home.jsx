60478027f11778ca99e7a22112bf839be9048c53
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-03-12 23:48:32 +0100

    finish the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 function Home() {
   return (
-    <div className="bg-home bg-cover h-screen w-full bg-center bg-no-repeat brightness-50 flex justify-center items-center">
-      <div className="bg-red-600 flex flex-col justify-center items-center">
-        <p className="text-white text-3xl">{`HI, I'M HOUSSAM BENLAGHA`}</p>
-        <p>React Developer.</p>
-        <div className="flex flex-col md:flex-row">
-          <button>PROJECTS</button>
+    <div className="bg-home bg-cover h-screen w-full bg-center bg-no-repeat ">
+      <div className="bg-[#000000bd] w-full h-screen flex justify-center items-center">
+        <div className=" flex flex-col justify-center items-center gap-3 ">
+          <p className="text-white text-xl">{`HI, I'M HOUSSAM BENLAGHA`}</p>
+          <p className="text-white text-5xl font-bold w-60 text-center  leading-snug md:w-auto md:text-6xl lg:text-7xl md:mb-2">
+            React Developer.
+          </p>
+          <div className="flex flex-row gap-2 ">
+            <button className="bg-yellow-300 border-2 border-yellow-300 py-3 px-10 text-sm font-semibold hover:bg-[#8b8b8b82] hover:border-white hover:text-white">
+              PROJECTS
+            </button>
 
-          <button>CONTACT</button>
+            <button className="border-2 py-3 px-10 text-white text-sm font-semibold hover:bg-[#8b8b8b82]">
+              CONTACT
+            </button>
+          </div>
         </div>
       </div>
     </div>
