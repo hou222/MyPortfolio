471c5229e8280b34ba28ee530d00b2c0ca7bfb07
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-06-09 11:16:35 +0100

    add furniture project link

--- src/features/Projects.jsx
+++ src/features/Projects.jsx
@@ -1,25 +1,36 @@
 import ProjectDetails from "./projects/ProjectDetails";
 import project1 from "../data/project1.png";
+import project2 from "../data/project22.png";
 
 function Projects() {
-  const urlProject = "https://tempoweather.netlify.app/";
-  const urlCode = "https://github.com/hou222/weatherApp";
+  const projects = [
+    {
+      live: "https://tempoweather.netlify.app/",
+      code: "https://github.com/hou222/weatherApp",
+      image: project1,
+    },
+    {
+      live: "https://vercel.com/hou222s-projects/furniture-world",
+      code: "https://github.com/hou222/FurnitureWorld",
+      image: project2,
+    },
+  ];
+
   return (
     <div className="bg-black lg:h-screen py-11 flex flex-col  justify-center items-center">
       <p className="text-white font-bold text-3xl text-center  pb-11">
         Projects
       </p>
-      <div className=" w-fit mb-11 grid grid-cols-1 gap-7 lg:gap-10 md:grid-cols-1 xl:grid-cols-1 ">
+      <div className=" w-fit mb-11 grid grid-cols-1 gap-7 lg:gap-10 md:grid-cols-2 xl:grid-cols-2 ">
         {/* <div className=" w-fit mb-11 grid grid-cols-1 gap-7 lg:gap-10 md:grid-cols-2 xl:grid-cols-3 "> */}
-        <ProjectDetails
-          imageName={project1}
-          projectLink={urlProject}
-          codeLink={urlCode}
-        >
-          Tempo is a straightforward web application that allows users to check
-          weather forecasts for any city <br></br>
-          <br></br> built with : HTML, CSS, Java Script
-        </ProjectDetails>
+        {projects.map((project) => (
+          <ProjectDetails key={project.code} project={project}>
+            Tempo is a straightforward web application that allows users to
+            check weather forecasts for any city <br></br>
+            <br></br> built with : HTML, CSS, Java Script
+          </ProjectDetails>
+        ))}
+
         {/* <ProjectDetails imageName={project2} />
         <ProjectDetails imageName={project3} /> */}
       </div>
--- src/features/projects/ProjectDetails.jsx
+++ src/features/projects/ProjectDetails.jsx
@@ -1,10 +1,8 @@
-function ProjectDetails({ imageName, projectLink, codeLink, children }) {
+function ProjectDetails({ project, children }) {
   return (
     <div className="bg-gray-800 w-[350px] md:w-[350px] lg:w-[400px]  p-3 flex flex-col gap-2">
-      <img src={imageName} alt="project photo" className="h-46 w-full" />
-      <h1 className="text-white font-semibold text-xl">
-        LMS/Course Website Design
-      </h1>
+      <img src={project.image} alt="project photo" className="h-46 w-full" />
+      <h1 className="text-white font-semibold text-xl">{`"Tempo" Weather App`}</h1>
       <p className="text-white tracking-widest">{children}</p>
       <div
         className="flex
@@ -12,7 +10,7 @@ function ProjectDetails({ imageName, projectLink, codeLink, children }) {
       >
         {/* <Button type="small">VIEW</Button> */}
         <a
-          href={projectLink}
+          href={project.live}
           target="_blank"
           rel="noopener noreferrer"
           className="bg-yellow-300 border-2 border-[#bec0ae] px-5 py-2 text-xs font-semibold hover:bg-[#8b8b8b82] hover:border-white hover:text-white hover:transition-all hover:duration-500 cursor-pointer"
@@ -20,7 +18,7 @@ function ProjectDetails({ imageName, projectLink, codeLink, children }) {
           VIEW
         </a>
         <a
-          href={codeLink}
+          href={project.code}
           target="_blank"
           rel="noopener noreferrer"
           className="bg-yellow-300 border-2 border-[#bec0ae] px-5 py-2 text-xs font-semibold hover:bg-[#8b8b8b82] hover:border-white hover:text-white hover:transition-all hover:duration-500 cursor-pointer"
